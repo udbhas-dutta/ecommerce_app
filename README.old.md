373aeaa1b5f2f9a76f19ab1148fca234c6a8a720
Udbhas Dutta <udbhasdutta@gmail.com>
2023-10-29 23:38:58 +0530

    set up the create-react app and tailwind css. Installed a few other dependencies.

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# ecommerce_app
+This is a repo of an ecommerce app made using MERN stack.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-gray-700 font-bold">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
