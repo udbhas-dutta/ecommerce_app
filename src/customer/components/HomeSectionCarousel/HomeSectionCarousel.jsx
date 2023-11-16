36ed40243cbaf97142d08d832a30fc2a1df2b922
Udbhas Dutta <udbhasdutta@gmail.com>
2023-11-16 19:55:20 +0530

    committed the last version to shift the repo to laptop

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -3,15 +3,22 @@ import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import { Button } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { useState } from "react";
 
 const HomeSectionCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  const slidePrev = ()=> setActiveIndex(activeIndex-1);
+  const slideNext = ()=> setActiveIndex(activeIndex+1);
+
+  const syncActiveIndex = ({item})=> setActiveIndex(item)
+  
+  const items = [1, 1, 1, 1, 1,1, 1, 1, 1, 1,1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
 
   return (
     <div className="px-4 lg:px-8">
@@ -22,10 +29,13 @@ const HomeSectionCarousel = () => {
           infinite
           responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
         />
-        <Button
+        {activeIndex !== items.length-5 && <Button
           variant="contained"
           className="z-50 bg-white"
+          onClick={slideNext}
           sx={{
             position: "absolute",
             top: "8rem",
@@ -38,11 +48,12 @@ const HomeSectionCarousel = () => {
           <KeyboardArrowLeftIcon
             sx={{ transform: "rotate(90deg)", color: "black" }}
           />
-        </Button>
+        </Button>}
 
         <Button
           variant="contained"
           className="z-50 bg-white"
+          onClick={slidePrev}
           sx={{
             position: "absolute",
             top: "8rem",
